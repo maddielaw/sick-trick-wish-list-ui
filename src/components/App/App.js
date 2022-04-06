245fdff5d1b57d75b6069174afd2a0b83063a580
Madison Law <madisonlaw1@gmail.com>
2022-04-06 09:50:26 -0600

    Add fetch call and instance of TrickContainer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,30 @@
 import { Component } from 'react';
 import './App.css';
+import Form from '../Form/Form'
+import fetchTricks from '../../apiCalls';
+import TrickContainer from '../TrickContainer/TrickContainer';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      tricks: []
+    }
+  }
+
+  componentDidMount = () => {
+    fetchTricks()
+      .then(data => {
+        this.setState({ tricks: data})
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+        <Form />
+        <TrickContainer tricks={this.state.tricks}/>
       </div>
     );
   }
