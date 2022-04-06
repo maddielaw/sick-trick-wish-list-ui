8bf0cdf3a07769d56ae7f3eebae9366e619929db
Madison Law <madisonlaw1@gmail.com>
2022-04-06 11:28:07 -0600

    Add POST method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 const fetchTricks = () => {
-  return fetch('http://localhost:3001/api/v1/tricks	')
+  return fetch('http://localhost:3001/api/v1/tricks')
     .then(response => {
       if (!response.ok) {
         throw new Error("Something went wrong! Please try again later.")
@@ -9,5 +9,22 @@ const fetchTricks = () => {
     })
 }
 
+const postTrick = (newTrick) => {
+  return fetch('http://localhost:3001/api/v1/tricks', {
+    method: "POST",
+    body: JSON.stringify(newTrick),
+    headers: { "Content-Type": "application/json" },
+  }).then(response => {
+    if(!response.ok) {
+      throw new Error("Please submit a trick with all form fields filled out.")
+    } else {
+      return response.json()
+    }
+  })
+}
 
-export default fetchTricks;
+
+
+
+
+export { fetchTricks, postTrick };
