f4afafba0dfcc60c44ed750e7a5bc79229eb44be
Madison Law <madisonlaw1@gmail.com>
2022-04-06 09:56:28 -0600

    Add uppercase formatter

--- src/components/TrickCard/TrickCard.js
+++ src/components/TrickCard/TrickCard.js
@@ -2,11 +2,17 @@ import React from 'react';
 import '../TrickCard/TrickCard.css'
 
 const TrickCard = ({ stance, name, obstacle, tutorial}) => {
+
+  const formatUppercase = (word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+
+  }
   return (
     <div className="trick-card">
-      <p>{stance}{name}</p>
-      <p>{`Obstacle: ${obstacle}`}</p>
-      <p>{`Link to tutorial: ${tutorial}`}</p>
+      <p>{formatUppercase(stance)} {formatUppercase(name)}</p>
+      <p>{`Obstacle: ${formatUppercase(obstacle)}`}</p>
+      <p>Link to Tutorial:</p>
+      <p>{tutorial}</p>
     </div>
   )
 
