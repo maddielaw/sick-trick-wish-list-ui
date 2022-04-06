e074c0f0f9454cd6d98ca010e0027ed0f2fa0124
Madison Law <madisonlaw1@gmail.com>
2022-04-06 10:41:17 -0600

    Remove keys from trick cards

--- src/components/TrickContainer/TrickContainer.js
+++ src/components/TrickContainer/TrickContainer.js
@@ -6,7 +6,7 @@ const TrickContainer = ({tricks}) => {
 
   const listTricks = (tricks) => {
     return tricks.map(trick => {
-      return <TrickCard key={Date.now()} stance={trick.stance} name={trick.name} obstacle={trick.obstacle} tutorial={trick.tutorial} />
+      return <TrickCard stance={trick.stance} name={trick.name} obstacle={trick.obstacle} tutorial={trick.tutorial} />
     })
 
   }
